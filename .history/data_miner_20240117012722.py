4bd2f36beb0391fb796974696462651533a47e8e
andylvu <andyvu95@gmail.com>
2024-01-22 00:18:10 -0500

    1/21

--- .history/data_miner_20240117012722.py
+++ .history/data_miner_20240117012722.py
@@ -11,7 +11,7 @@ def main():
         data_miner = DataMining()
 #       data_miner.create_table() already created
     
-        for _ in range(105):
+        for _ in range(3):
 
               
             whole_image = data_miner.screenshot()
